7903ef834d90aa7025c6ad261da6cf1f8795a6e5
HARSHAL SAHADEOKAR <131584127+harshal1996sahadeokar@users.noreply.github.com>
2025-05-01 13:03:55 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,4 +1,9 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(3000);
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello to the HarshalTECH! India's Largest StartUp'"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
